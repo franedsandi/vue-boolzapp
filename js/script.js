2e8a4095d8f52952a72c0a66081909cfaeeea60c
franedsandi <franedsandi@gmail.com>
2023-10-03 14:54:54 +0200

    [feat] receive message ok!

--- js/script.js
+++ js/script.js
@@ -29,17 +29,29 @@ createApp({
     parseDate(dateString) {
       const [datePart, timePart] = dateString.split(' ');
       const [day, month, year] = datePart.split('/');
-      const [hours, minutes, seconds] = timePart.split(':');
-      return new Date(year, month - 1, day, hours, minutes, seconds);
+      const [hours, minutes] = timePart.split(':');
+      return new Date(year, month - 1, day, hours, minutes);
     },
     getMessageTime(message) {
       return this.formatDate(message.date);
     },
     getLastMessageTime(contact) {
       const lastMessage = contact.messages[contact.messages.length - 1];
-      return lastMessage ? this.formatDate(lastMessage.date) : '';
+      return lastMessage ? this.formatTime(lastMessage.date) : '';
     },
     formatDate(dateString) {
+      if (dateString) {
+        const messageDate = this.parseDate(dateString);
+        const day = messageDate.getDate();
+        const month = messageDate.getMonth() + 1;
+        const year = messageDate.getFullYear();
+        const hours = messageDate.getHours();
+        const minutes = messageDate.getMinutes();
+        return `${String(day).padStart(2, '0')}/${String(month).padStart(2, '0')}/${year} ${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+      }
+      return '';
+    },
+    formatTime(dateString) {
       if (dateString) {
         const messageDate = this.parseDate(dateString);
         const hours = messageDate.getHours();
@@ -67,17 +79,28 @@ createApp({
     sendMessage() {
       if (this.selectedContact && this.newMessageText) {
         const currentTime = new Date();
+        const formattedDate = `${String(currentTime.getDate()).padStart(2, '0')}/${String(currentTime.getMonth() + 1).padStart(2, '0')}/${currentTime.getFullYear()}`;
         const formattedTime = `${String(currentTime.getHours()).padStart(2, '0')}:${String(currentTime.getMinutes()).padStart(2, '0')}`;
+        const formattedDateTime = `${formattedDate} ${formattedTime}`;
+        
         const newMessage = {
-          date: currentTime.toLocaleString(),
+          date: formattedDateTime,
           message: this.newMessageText,
           status: 'sent',
         };
         this.selectedContact.messages.push(newMessage);
         this.newMessageText = '';
 
+        setTimeout(() => {
+          const receivedMessage = {
+            date: formattedDateTime,
+            message: 'Ok!',
+            status: 'received',
+          };
+          this.selectedContact.messages.push(receivedMessage);
+        }, 1000);
       }
-    },
+    }
   },
   computed: {
     sortedMessages() {
@@ -90,3 +113,4 @@ createApp({
     },
   },
 }).mount('#app');
+
